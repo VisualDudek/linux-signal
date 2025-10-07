2aae44de16240bf8f3bba0eb6a79c35fa5f35628
MarcinDudek <dudek.v@gmail.com>
2025-10-07 11:46:43 +0200

    Add 013_lab_sleep.sh script to demonstrate process management with strace

---
+++ 013_lab_sleep.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Script PID: $$"
+
+strace -f -e trace=signal,fork,vfork,clone,execve,setpgid bash <<EOF
+echo "Bash PID: $$"
+# echo "Bash PGID: $(ps -o pgid= -p $$)"
+sleep 1
+EOF
+
